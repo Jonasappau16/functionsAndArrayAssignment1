78816318783c548b282685268297979e864103b8
J@y14 <jonasappau16@gmail.com>
2022-07-12 23:00:26 -0400

    Functions and Array

---
+++ app.js
@@ -0,0 +1,42 @@
+// function that returns the sum of two numbers
+
+var x = 7;
+var y = 7;
+
+function sum(x , y){
+    console.log(x + y);
+}
+ sum(x, y);
+
+
+
+// function that returns two arguments multiply by each other. 
+
+var n = 7
+var k = 2
+ function multiply(k , n){
+    console.log(k * n);
+}
+ multiply(n, k);
+
+
+ // function that returns the difference of two numbers that are arguments
+
+
+ var a = 21;
+var b = 7;
+ function myFunction(a , b){
+    console.log(a - b);
+}
+ myFunction(a, b);
+
+// division
+
+var c = 28;
+var d = 2;
+ function myDivision(c , d){
+    console.log(c / d);
+}
+ myDivision(c, d);
+
+
